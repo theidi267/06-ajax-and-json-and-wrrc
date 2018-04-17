2632e3178205d951351ecf6db358c42a318c9e25
Katy Bowers <katyvine@gmail.com>
2018-04-17 11:17:34 -0700

    fill out comments and add README

--- katy-timea/scripts/article.js
+++ katy-timea/scripts/article.js
@@ -13,15 +13,14 @@ function Article (rawDataObj) {
 Article.all = [];
 
 // COMMENT: Why isn't this method written as an arrow function?
-// PUT YOUR RESPONSE HERE
+// We can't use an arrow function here because the function is using 'contextual this'. Arrow functions to not have their own 'this'.
 Article.prototype.toHtml = function() {
   let template = Handlebars.compile($('#article-template').text());
 
   this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
 
   // COMMENT: What is going on in the line below? What do the question mark and colon represent? How have we seen this same logic represented previously?
-  // Not sure? Check the docs!
-  // PUT YOUR RESPONSE HERE
+  // the line is checking to see if there is data in the 'publishedOn' field. If it's empty then it assigns the selector as draft to hide the post until published.
   this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
   this.body = marked(this.body);
 
@@ -33,7 +32,7 @@ Article.prototype.toHtml = function() {
 // REVIEW: This function will take the rawData, how ever it is provided, and use it to instantiate all the articles. This code is moved from elsewhere, and encapsulated in a simply-named function for clarity.
 
 // COMMENT: Where is this function called? What does 'rawData' represent now? How is this different from previous labs?
-// PUT YOUR RESPONSE HERE
+// It's called in the fetchAll function right below this one. 'rawData' is the contents of the JSON file, replacing the articleData.js file from previous labs.
 Article.loadAll = articleData => {
   articleData.sort((a,b) => (new Date(b.publishedOn)) - (new Date(a.publishedOn)))
 
@@ -43,6 +42,10 @@ Article.loadAll = articleData => {
 // REVIEW: This function will retrieve the data from either a local or remote source, and process it, then hand off control to the View.
 Article.fetchAll = () => {
   // REVIEW: What is this 'if' statement checking for? Where was the rawData set to local storage?
+
+  // COMMENT: sequence of code...
+  // First we check to see if the data is in local storge from a previous page load. If it's there we render the articles through local storage.
+  // Second, if there's not local storage we go to the else statement where we load up the data from the JSON file and set it to storage for use the next time we load the page.
   if (localStorage.rawData) {
 
     Article.loadAll(JSON.parse(localStorage.rawData));
